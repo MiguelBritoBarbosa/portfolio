6d51ebd49f7f79656c955c1ce4a18c1fdf719e73
MiguelBritoBarbosa <107328582+MiguelBritoBarbosa@users.noreply.github.com>
2024-07-04 17:15:41 -0300

    removing unnecessary component

--- front-end/src/components/Heading/styled.ts
+++
@@ -1,8 +0,0 @@
-'use client';
-import { theme } from '@/styles/theme';
-import styled from 'styled-components';
-import { HeadingProps } from '.';
-
-export const Title = styled.h1<HeadingProps>`
-    color: ${theme.colors.primary};
-`;
