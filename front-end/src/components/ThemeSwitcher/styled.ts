e94651f3833c357c0dd3a9a40c656f180124ea69
MiguelBritoBarbosa <107328582+MiguelBritoBarbosa@users.noreply.github.com>
2024-07-15 21:01:19 -0300

    adding styles to ThemeSwitcher

--- front-end/src/components/ThemeSwitcher/styled.ts
+++ front-end/src/components/ThemeSwitcher/styled.ts
@@ -1,4 +1,8 @@
 'use client';
 import styled from 'styled-components';
 
-export const SwitchThemeButton = styled.button``;
+export const SwitchThemeButton = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
