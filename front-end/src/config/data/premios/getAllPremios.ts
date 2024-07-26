309009dc45b6c3f9dec04fd10e6dbd0335779b98
MiguelBritoBarbosa <107328582+MiguelBritoBarbosa@users.noreply.github.com>
2024-07-26 20:30:35 -0300

    fixing getAllPremios

--- front-end/src/config/data/premios/getAllPremios.ts
+++ front-end/src/config/data/premios/getAllPremios.ts
@@ -1,11 +1,11 @@
-import { DataUndefined } from '@/config/domain/dataUndefined';
-import { ProjetoData } from '@/config/domain/projetos/projetos';
+import { PremioData } from '@/config/domain/premios/premios';
 import { PREMIOS_URL } from '@/config/siteConfig';
 import { fetchJson } from '@/utils/fetchJson';
+import { getLocale } from 'next-intl/server';
 
-export async function getAllPremios(query = ''): Promise<ProjetoData[] | DataUndefined> {
-    const locale = 'pt-BR';
+export async function getAllPremios(query = ''): Promise<{ data: PremioData[] } | undefined> {
+    const locale = await getLocale();
     const url = `${PREMIOS_URL}&locale=${locale}&${query}`;
-    const premios: ProjetoData[] = await fetchJson(url);
-    return premios ? premios : { data: [undefined] };
+    const premios: { data: PremioData[] } = await fetchJson(url);
+    return premios ? (premios.data ? premios : undefined) : undefined;
 }
