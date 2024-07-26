a1d16fbaa21cf70465a94f72af9e56a8961d0c4a
MiguelBritoBarbosa <107328582+MiguelBritoBarbosa@users.noreply.github.com>
2024-07-26 20:15:20 -0300

    fixing premio slider

--- front-end/src/components/PremiosSlider/index.tsx
+++ front-end/src/components/PremiosSlider/index.tsx
@@ -3,141 +3,123 @@ import Image from 'next/image';
 import { Container } from './styled';
 import { PremioData } from '@/config/domain/premios/premios';
 import { API_ROOT } from '@/config/siteConfig';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Link from 'next/link';
 import { rgbDataURL } from '@/utils/rgbDataUrl';
+import { Button, Heading } from '@radix-ui/themes';
+import { AwardsData } from '@/config/domain/pages/sections/awards';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper as SwiperType } from 'swiper';
+import { useTranslations } from 'next-intl';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 interface PremiosSliderProps {
     premios: PremioData[];
     predominantColors: [number[]];
+    sectionData: AwardsData;
 }
 
-export const PremiosSlider = ({ premios, predominantColors }: PremiosSliderProps) => {
+export const PremiosSlider = ({ premios, predominantColors, sectionData }: PremiosSliderProps) => {
+    const t = useTranslations('Sections.Awards');
     const url = premios[0].attributes.cover.data.attributes.formats.large
         ? premios[0].attributes.cover.data.attributes.formats.large.url
         : premios[0].attributes.cover.data.attributes.url;
     const [backgroundImage, setBackgroundImage] = useState(url);
-    const [carouselItens]: any = useState([]);
-    useEffect(() => {
-        carouselItens.map((item: any) => {
-            const div: any = document.getElementById(item.props.id);
-            const observer = new MutationObserver((mutation) => {
-                const target: any = mutation[0].target;
-                if (target.classList.contains('active')) {
-                    premios.map((premio) => {
-                        if (premio.attributes.slug === target.id) {
-                            const url = premios[0].attributes.cover.data.attributes.formats.large
-                                ? premios[0].attributes.cover.data.attributes.formats.large.url
-                                : premios[0].attributes.cover.data.attributes.url;
-                            setBackgroundImage(url);
-                        }
-                    });
-                }
-            });
-            observer.observe(div, { attributes: true });
-        });
-    });
+
+    const changeBackground = (target: SwiperType) => {
+        const url = premios[target.activeIndex].attributes.cover.data.attributes.formats.large
+            ? premios[target.activeIndex].attributes.cover.data.attributes.formats.large.url
+            : premios[target.activeIndex].attributes.cover.data.attributes.url;
+        setBackgroundImage(url);
+    };
 
     return (
         <Container
-            className="row my-5"
             style={{
                 backgroundImage: `url(${API_ROOT}${backgroundImage})`,
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
                 backgroundRepeat: 'no-repeat',
-                maxHeight: '800px',
             }}
+            className="transition-all"
         >
-            <div
-                id="carousel-container"
-                className=" d-flex justify-content-center p-4"
-                style={{ backdropFilter: 'blur(10px)' }}
-            >
-                <div id="carousel" className=" carousel slide carousel-dark" data-bs-ride="carousel">
-                    <div className="carousel-indicators">
-                        {premios.map((premio, index) => {
-                            return (
-                                <button
-                                    key={index}
-                                    className="active"
-                                    type="button"
-                                    data-bs-target="#carousel"
-                                    data-bs-slide-to={index}
-                                    aria-current="true"
-                                    aria-label={`Slide ${index + 1}`}
-                                ></button>
-                            );
-                        })}
-                    </div>
-                    <div className="carousel-inner rounded-3">
-                        {premios.map((premio, index) => {
-                            const description: any = premio.attributes.descricao[0].children[0];
-                            const url = `${API_ROOT}${premio.attributes.cover.data.attributes.formats.medium ? premio.attributes.cover.data.attributes.formats.medium.url : premio.attributes.cover.data.attributes.url}`;
-                            const predominantColor: number[] = predominantColors[index];
-                            const width = premio.attributes.cover.data.attributes.formats.medium
-                                ? premio.attributes.cover.data.attributes.formats.medium.width
-                                : premio.attributes.cover.data.attributes.width;
-                            const height = premio.attributes.cover.data.attributes.formats.medium
-                                ? premio.attributes.cover.data.attributes.formats.medium.height
-                                : premio.attributes.cover.data.attributes.height;
-                            const slide = (
+            <div className="py-8 backdrop-blur-md">
+                <Heading
+                    as="h2"
+                    color="violet"
+                    className="mb-4 text-center bg-gray-200 dark:bg-gray-800 rounded mx-auto w-min p-1"
+                >
+                    {sectionData.metadados.nome}
+                </Heading>
+                <Swiper
+                    navigation={true}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Navigation, Pagination]}
+                    className="awardsSwiper"
+                    onSlideChange={function (this: any) {
+                        changeBackground(this);
+                    }}
+                    slidesPerView={1}
+                    spaceBetween={30}
+                    autoplay={{
+                        delay: 4000,
+                    }}
+                >
+                    {premios.map((premio, index) => {
+                        const description: any = premio.attributes.descricao[0].children[0];
+                        const url = `${API_ROOT}${premio.attributes.cover.data.attributes.formats.medium ? premio.attributes.cover.data.attributes.formats.medium.url : premio.attributes.cover.data.attributes.url}`;
+                        const predominantColor: number[] = predominantColors[index];
+                        const width = premio.attributes.cover.data.attributes.formats.medium
+                            ? premio.attributes.cover.data.attributes.formats.medium.width
+                            : premio.attributes.cover.data.attributes.width;
+                        const height = premio.attributes.cover.data.attributes.formats.medium
+                            ? premio.attributes.cover.data.attributes.formats.medium.height
+                            : premio.attributes.cover.data.attributes.height;
+                        return (
+                            <SwiperSlide key={premio.attributes.slug} className="!flex justify-center p-2">
                                 <div
-                                    key={premio.attributes.slug}
-                                    id={premio.attributes.slug}
-                                    className={`carousel-item ${index === 0 ? 'active' : ''}`}
+                                    className="grid border bg-gray-200 dark:bg-gray-800 border-1 border-gray-300 rounded"
+                                    style={{ maxWidth: width }}
                                 >
-                                    <Image
-                                        className="d-md-block img-fluid"
-                                        src={url}
-                                        placeholder="blur"
-                                        blurDataURL={rgbDataURL(
-                                            predominantColor[0],
-                                            predominantColor[1],
-                                            predominantColor[2],
-                                        )}
-                                        alt={premio.attributes.slug}
-                                        width={width}
-                                        height={height}
-                                    />
-                                    <div className="bg-light rounded carousel-caption d-none d-md-block">
+                                    <Link href={`/premios/${premio.attributes.slug}`}>
+                                        <Image
+                                            src={url}
+                                            placeholder="blur"
+                                            blurDataURL={rgbDataURL(
+                                                predominantColor[0],
+                                                predominantColor[1],
+                                                predominantColor[2],
+                                            )}
+                                            alt={premio.attributes.slug}
+                                            width={width}
+                                            height={height}
+                                        />
+                                    </Link>
+
+                                    <div className="p-4 md:p-6">
                                         <h5 className="fs-5">{premio.attributes.titulo}</h5>
                                         <div className="fw-bold mb-3">
                                             <div className="d-flex px-1">
                                                 <span className="d-block tw-text-justify">
-                                                    {description.text.split(' ').splice(0, 15).join(' ')}
-                                                    {description.text.split(' ').length > 15 ? <>...</> : <></>}
+                                                    {description.text.split(' ').splice(0, 24).join(' ')}
+                                                    {description.text.split(' ').length > 24 ? <>...</> : <></>}
                                                 </span>
                                             </div>
                                         </div>
-                                        <Link href={`/premios/${premio.attributes.slug}`} className="btn btn-primary">
-                                            Veja Mais
-                                        </Link>
-                                    </div>
-                                    <div className="carousel-caption d-block d-md-none fs-1">
-                                        <Link
-                                            className="text-decoration-underline ver-mais"
-                                            href={`/premios/${premio.attributes.slug}`}
-                                        >
-                                            Acesse
-                                        </Link>
+                                        <Button color="violet">
+                                            <Link href={`/premios/${premio.attributes.slug}`}>{t('See more')}</Link>
+                                        </Button>
                                     </div>
                                 </div>
-                            );
-                            carouselItens.push(slide);
-                            return slide;
-                        })}
-                    </div>
-                </div>
-
-                <button className="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
+                            </SwiperSlide>
+                        );
+                    })}
+                </Swiper>
             </div>
         </Container>
     );
--- front-end/src/components/PremiosSlider/styled.ts
+++ front-end/src/components/PremiosSlider/styled.ts
@@ -3,20 +3,6 @@ import { theme } from '@/styles/theme';
 import styled from 'styled-components';
 
 export const Container = styled.section`
-    #servicos {
-        border-bottom: 2px solid ${theme.colors.primary};
-        width: 10%;
-    }
-
-    .btn-primary {
-        background-color: ${theme.colors.primary};
-        border-color: ${theme.colors.primary};
-    }
-
-    .btn-primary:hover {
-        background-color: ${theme.colors.secondary};
-        border-color: ${theme.colors.secondary};
-    }
     .ver-mais {
         color: ${theme.colors.primary};
     }
