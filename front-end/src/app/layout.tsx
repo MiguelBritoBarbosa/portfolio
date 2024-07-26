a41f686c5e8d9c4ba6fac9d435da47d2d645affd
MiguelBritoBarbosa <107328582+MiguelBritoBarbosa@users.noreply.github.com>
2024-07-26 20:13:19 -0300

    updating layout

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -1,18 +1,25 @@
 import '@/styles/globals.css';
-import StyledComponentsRegistry from '../registry';
+import StyledComponentsRegistry from './registry';
 import { Header } from '@/components/Header';
 import { Footer } from '@/components/Footer';
 import NprogressComponent from '@/config/nprogress';
 import '@/styles/nprogress.css';
+import { getHeader } from '@/config/data/header/getHeader';
+import { ThemeProvider } from '../providers/theme-provider';
+import { Theme } from '@radix-ui/themes';
+import { headerData } from '@/config/domain/header/header';
+import { NextIntlClientProvider } from 'next-intl';
+import { getPredominantColor } from '@/utils/getPredominantColor';
+import { API_ROOT } from '@/config/siteConfig';
+import { footerData } from '@/config/domain/footer/footer';
+import { getFooter } from '@/config/data/footer/getFooter';
+import { getLocale, getMessages } from 'next-intl/server';
 import { Montserrat } from 'next/font/google';
 const montserrat = Montserrat({
     subsets: ['latin'],
     weight: ['500', '600', '700', '800', '900'],
     variable: '--font-montserrat',
 });
-import { getHeader } from '@/config/data/header/getHeader';
-import { ThemeProvider } from '../providers/theme-provider';
-import { Theme } from '@radix-ui/themes';
 
 export async function generateMetadata() {
     const header: { data: headerData } | undefined = await getHeader();
@@ -28,49 +35,21 @@ export async function generateMetadata() {
     };
 }
 
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/pagination';
-import { headerData } from '@/config/domain/header/header';
-import { notFound } from 'next/navigation';
-import { NextIntlClientProvider } from 'next-intl';
-import { getPredominantColor } from '@/utils/getPredominantColor';
-import { API_ROOT } from '@/config/siteConfig';
-import { footerData } from '@/config/domain/footer/footer';
-import { getFooter } from '@/config/data/footer/getFooter';
-import { unstable_setRequestLocale } from 'next-intl/server';
-
-export function generateStaticParams() {
-    return [{ locale: 'en' }, { locale: 'pt' }];
-}
-
 interface Props {
     children: React.ReactNode;
-    params: {
-        locale: string;
-    };
 }
-export default async function RootLayout({ children, params: { locale } }: Props) {
-    let messages;
-    try {
-        messages = (await import(`../../messages/${locale}.json`)).default;
-    } catch (error) {
-        notFound();
-    }
-
-    unstable_setRequestLocale(locale);
-
+export default async function RootLayout({ children }: Props) {
+    const locale = await getLocale();
+    const messages = await getMessages();
     const header: { data: headerData } | undefined = await getHeader();
     const predominantColor = await getPredominantColor(
         `${API_ROOT}${header?.data.attributes.banner.data.attributes.url}`,
     );
     const footer: { data: footerData } | undefined = await getFooter();
-
     return (
         <html lang={locale}>
             <body className={montserrat.className}>
-                <NextIntlClientProvider locale={locale} messages={messages}>
+                <NextIntlClientProvider messages={messages}>
                     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
                         <StyledComponentsRegistry>
                             <Theme accentColor="violet">
@@ -79,7 +58,7 @@ export default async function RootLayout({ children, params: { locale } }: Props
                                     headerData={header !== undefined ? header.data : null}
                                     bannerColor={predominantColor}
                                 />
-                                <main>{children}</main>
+                                {children}
                                 <Footer footerData={footer !== undefined ? footer.data : null} />
                             </Theme>
                         </StyledComponentsRegistry>
