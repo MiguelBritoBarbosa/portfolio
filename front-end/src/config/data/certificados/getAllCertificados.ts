8bba4723890545c81974285c672b5d6b71ba9298
MiguelBritoBarbosa <107328582+MiguelBritoBarbosa@users.noreply.github.com>
2024-07-26 23:01:02 -0300

    fixing getAllCertificados

--- front-end/src/config/data/certificados/getAllCertificados.ts
+++ front-end/src/config/data/certificados/getAllCertificados.ts
@@ -1,11 +1,11 @@
 import { CertificadoData } from '@/config/domain/certificados/certificados';
-import { DataUndefined } from '@/config/domain/dataUndefined';
 import { CERTIFICADOS_URL } from '@/config/siteConfig';
 import { fetchJson } from '@/utils/fetchJson';
+import { getLocale } from 'next-intl/server';
 
-export async function getAllCertificados(query = ''): Promise<CertificadoData[] | DataUndefined> {
-    const locale = 'pt-BR';
+export async function getAllCertificados(query = ''): Promise<{ data: CertificadoData[] } | undefined> {
+    const locale = await getLocale();
     const url = `${CERTIFICADOS_URL}&locale=${locale}&${query}`;
-    const certificados: CertificadoData[] = await fetchJson(url);
-    return certificados !== null ? certificados : { data: [undefined] };
+    const certificados: { data: CertificadoData[] } = await fetchJson(url);
+    return certificados ? (certificados.data ? certificados : undefined) : undefined;
 }
