00bb98aadf59e775d2a80ba68a4d881c3b8bc125
MiguelBritoBarbosa <107328582+MiguelBritoBarbosa@users.noreply.github.com>
2024-08-05 08:19:45 -0300

    creating autores archive

--- front-end/src/app/autores/page.tsx
+++ front-end/src/app/autores/page.tsx
@@ -1,7 +1,22 @@
+import { getAllAutores } from '@/config/data/autores/getAllAutores';
+import { AutorData } from '@/config/domain/autores/autores';
+import Autores from '@/containers/Autores';
+import { Heading } from '@radix-ui/themes';
+import { getTranslations } from 'next-intl/server';
+
+export const revalidate = 300;
+
 export default async function AutoresPage() {
+    const t = await getTranslations('Pages.Authors');
+    const autores: { data: AutorData[] } | undefined = await getAllAutores('&populate=*');
+    if (autores !== undefined && autores.data.length > 0) {
+        return <Autores autores={autores.data} />;
+    }
     return (
-        <>
-            <h1>AUTORES</h1>
-        </>
+        <div className="container mx-auto">
+            <Heading className="text-center mb-2" size={{ initial: '6', md: '7', lg: '8' }}>
+                {t('No authors found!')}
+            </Heading>
+        </div>
     );
 }
---
+++ front-end/src/containers/Autores/index.tsx
@@ -0,0 +1,25 @@
+import { Heading } from '@radix-ui/themes';
+import { getTranslations } from 'next-intl/server';
+import { Container } from './styled';
+import { CardAutorDestaque } from '@/components/CardAutorDestaque';
+import { AutorData } from '@/config/domain/autores/autores';
+
+interface AuthorsProps {
+    autores: AutorData[];
+}
+
+export default async function Autores({ autores }: AuthorsProps) {
+    const t = await getTranslations('Pages.Authors');
+    return (
+        <Container>
+            <Heading className="text-center my-2" size={{ initial: '6', md: '7', lg: '8' }}>
+                {t('Authors')}
+            </Heading>
+            <section className="container mx-auto py-8 grid md:grid-cols-2 lg:grid-cols-3 px-2 sm:px-4 gap-4 rounded bg-gray-300 dark:bg-gray-900">
+                {autores.map((autor) => {
+                    return <CardAutorDestaque key={`author-${autor.attributes.slug}`} autorDestaque={autor} />;
+                })}
+            </section>
+        </Container>
+    );
+}
