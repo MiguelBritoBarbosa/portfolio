3689ad6b84d9d1608f7ff69752341fe209a77712
MiguelBritoBarbosa <107328582+MiguelBritoBarbosa@users.noreply.github.com>
2024-08-09 20:30:32 -0300

    updating award page

--- front-end/src/containers/Premio/index.tsx
+++ front-end/src/containers/Premio/index.tsx
@@ -1,27 +1,49 @@
 import { PremioData } from '@/config/domain/premios/premios';
 import { Container } from './styles';
-import { CardPremio } from '@/components/CardPremio_old';
 import { API_ROOT } from '@/config/siteConfig';
-import { PremioDetails } from '@/components/PremioDetails';
 import { Heading } from '@radix-ui/themes';
+import { InternalPageContainer } from '@/components/InternalPageContainer';
+import { getPredominantColor } from '@/utils/getPredominantColor';
+import { InternalPageDetails } from '@/components/InternalPageDetails';
 
 export interface PremioProps {
     premio: PremioData;
 }
 
-export default function Premio({ premio }: PremioProps) {
+export default async function Premio({ premio }: PremioProps) {
+    let url;
+    let width;
+    let height;
+    if (
+        premio.attributes.cover.data.attributes.formats !== null &&
+        premio.attributes.cover.data.attributes.formats.medium !== undefined
+    ) {
+        url = `${API_ROOT}${premio.attributes.cover.data.attributes.formats.medium.url}`;
+        width = premio.attributes.cover.data.attributes.formats.medium.width;
+        height = premio.attributes.cover.data.attributes.formats.medium.height;
+    } else {
+        url = `${API_ROOT}${premio.attributes.cover.data.attributes.url}`;
+        width = premio.attributes.cover.data.attributes.width;
+        height = premio.attributes.cover.data.attributes.height;
+    }
+    const predominantColor: number[] = await getPredominantColor(url);
     return (
-        <Container id={premio.attributes.slug} className="container mt-3 p-5">
-            <Heading>{premio.attributes.titulo}</Heading>
-            <CardPremio
-                content={premio.attributes.descricao}
-                titulo={premio.attributes.titulo}
-                url={`${API_ROOT}${premio.attributes.cover.data.attributes.url}`}
-                width={premio.attributes.cover.data.attributes.formats.medium.width}
-                height={premio.attributes.cover.data.attributes.formats.medium.height}
-            />
-            <hr />
-            <PremioDetails date={premio.attributes.createdAt} autor={premio.attributes.autor.data} />
+        <Container className="">
+            <Heading className="text-center my-3" size={{ initial: '6', sm: '7', md: '8' }}>
+                {premio.attributes.titulo}
+            </Heading>
+            <section className="container mx-auto py-8 grid px-2 sm:px-4 gap-4 rounded bg-gray-300 dark:bg-gray-900 justify-center items-stretch">
+                <InternalPageContainer
+                    content={premio.attributes.descricao}
+                    titulo={premio.attributes.titulo}
+                    url={url}
+                    predominantColor={predominantColor}
+                    width={width}
+                    height={height}
+                />
+
+                <InternalPageDetails date={premio.attributes.createdAt} autores={[premio.attributes.autor.data]} />
+            </section>
         </Container>
     );
 }
