a0e20d1e65d81b4da49aeb495b039bda1426a040
MiguelBritoBarbosa <107328582+MiguelBritoBarbosa@users.noreply.github.com>
2024-08-09 22:01:06 -0300

    making award internal page

--- front-end/src/app/premios/[slug]/page.tsx
+++ front-end/src/app/premios/[slug]/page.tsx
@@ -1,16 +1,24 @@
 import { getPremio } from '@/config/data/premios/getPremio';
 import Premio from '@/containers/Premio';
+import { notFound } from 'next/navigation';
 
 export async function generateMetadata({ params }: { params: { slug: string } }) {
     const premio: any = await getPremio(params.slug);
-    return {
-        title: premio.data[0].attributes.Titulo,
-        description: premio.data[0].attributes.descricao[0].children[0].text.slice(0, 100),
-    };
+    if (premio !== undefined && premio.data.length > 0) {
+        return {
+            title: premio.data[0].attributes.titulo,
+            description: premio.data[0].attributes.descricao[0].children[0].text.slice(0, 160),
+        };
+    } else {
+        return notFound();
+    }
 }
 
 export default async function PremioPage({ params }: { params: { slug: string } }) {
-    const premio: any = await getPremio(params.slug);
+    const premio: any = await getPremio(
+        params.slug,
+        'populate[autor][fields][0]=nome&populate[autor][fields][1]=slug&populate[autor][populate][foto][fields][0]=*&populate[cover][fields]=*',
+    );
     return (
         <>
             <Premio premio={premio.data[0]} />
--- front-end/src/config/data/premios/getPremio.ts
+++ front-end/src/config/data/premios/getPremio.ts
@@ -3,9 +3,9 @@ import { PREMIOS_URL } from '@/config/siteConfig';
 import { fetchJson } from '@/utils/fetchJson';
 import { getLocale } from 'next-intl/server';
 
-export async function getPremio(slug: string | string[]): Promise<{ data: PremioData } | undefined> {
+export async function getPremio(slug: string, query: string = ''): Promise<{ data: PremioData[] } | undefined> {
     const locale = await getLocale();
-    const url = `${PREMIOS_URL}&locale=${locale}&filters[slug][$eq]=${slug}`;
-    const premio: { data: PremioData } = await fetchJson(url);
+    const url = `${PREMIOS_URL}&locale=${locale}&filters[slug][$eq]=${slug}&${query}`;
+    const premio: { data: PremioData[] } = await fetchJson(url);
     return premio ? (premio.data ? premio : undefined) : undefined;
 }
