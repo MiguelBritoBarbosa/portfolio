b21ccdc218b6f9da29f7e7cdad68d8fbeddb8299
MiguelBritoBarbosa <107328582+MiguelBritoBarbosa@users.noreply.github.com>
2024-08-11 19:40:22 -0300

    creating docker container

--- front-end/next.config.mjs
+++ front-end/next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'http',
-                hostname: 'localhost',
+                hostname: 'portifolio_backend',
                 port: '1337',
                 pathname: '/uploads/**',
             },
