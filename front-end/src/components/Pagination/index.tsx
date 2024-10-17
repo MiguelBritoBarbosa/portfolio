b43b30f1c299e10cd75007253fca97ce200b904c
MiguelBritoBarbosa <107328582+MiguelBritoBarbosa@users.noreply.github.com>
2024-10-17 08:45:31 -0300

    fixing pagination

--- front-end/src/components/Pagination/index.tsx
+++ front-end/src/components/Pagination/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { Container } from './styled';
-import Pagination from '@mui/material/Pagination';
-import PaginationItem from '@mui/material/PaginationItem';
+import usePagination from '@mui/material/usePagination';
+import { Button } from '@radix-ui/themes';
 import Link from 'next/link';
 
 interface PaginationContainerProps {
@@ -10,24 +10,51 @@ interface PaginationContainerProps {
 }
 
 export const PaginationContainer = ({ totalPage, page }: PaginationContainerProps) => {
+    const { items } = usePagination({
+        count: totalPage,
+    });
     return (
         <Container className="flex justify-center mt-8">
-            <Pagination
-                count={totalPage}
-                page={page}
-                variant="text"
-                shape="rounded"
-                renderItem={(item) => {
-                    return (
-                        <PaginationItem
-                            className=" rt-BaseButton rt-r-size-2 rt-variant-soft rt-Button cursor-pointer transition"
-                            component={Link}
-                            href={`?page=${item.page}`}
-                            {...item}
-                        />
-                    );
-                }}
-            />
+            <ul className="list-none flex gap-2">
+                {items.map(({ page, type, selected, ...item }, index) => {
+                    let children = null;
+                    if (type === 'start-ellipsis' || type === 'end-ellipsis') {
+                        children = '…';
+                    } else if (type === 'page') {
+                        children = (
+                            <Button
+                                variant="soft"
+                                className={`cursor-pointer transition ${selected ? 'bg-[rgba(0,0,0,0.08)]' : ''}`}
+                                {...item}
+                            >
+                                <Link href={`?page=${page}`}>{page}</Link>
+                            </Button>
+                        );
+                    } else {
+                        console.log(item);
+                        children = (
+                            <Button variant="soft" className="px-2 cursor-pointer transition" {...item}>
+                                {item.disabled ? (
+                                    type === 'next' ? (
+                                        <i className="bi bi-arrow-right-short flex leading-[0] text-xl"></i>
+                                    ) : (
+                                        <i className="bi bi-arrow-left-short flex leading-[0] text-xl"></i>
+                                    )
+                                ) : (
+                                    <Link href={`?page=${page}`}>
+                                        {type === 'next' ? (
+                                            <i className="bi bi-arrow-right-short flex leading-[0] text-xl"></i>
+                                        ) : (
+                                            <i className="bi bi-arrow-left-short flex leading-[0] text-xl"></i>
+                                        )}
+                                    </Link>
+                                )}
+                            </Button>
+                        );
+                    }
+                    return <li key={index}>{children}</li>;
+                })}
+            </ul>
         </Container>
     );
 };
